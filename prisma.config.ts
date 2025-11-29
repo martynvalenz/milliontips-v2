cef1c9f6532e1f25f202fe3baba624daaf04c1ac
Martín Valenzuela <martynvalenz@gmail.com>
2025-11-29 11:18:29 -0600

    first commit

---
+++ prisma.config.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import { defineConfig, env } from 'prisma/config'
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: '.env.local' });
+}
+
+export default defineConfig({
+  schema: './prisma/schema.prisma',
+  migrations: {
+    path: './prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+})
